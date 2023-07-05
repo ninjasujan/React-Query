4478bff93dee025f2cd64a2f784b6e7c2e948b02
ninjasujan <sujanb.poojary@gmail.com>
2023-07-05 23:39:51 +0530

    useQuery hook basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
-function App() {
+import { useQuery } from "@tanstack/react-query";
+
+/** Sample data */
+const POST = [
+  { title: "Post 1", location: "New Dehli" },
+  { title: "Post 2", location: "New banglore" },
+];
+
+const App = () => {
+  /** Delayed API call to simulate async fetch */
+  const wait = (
+    duration: number
+  ): Promise<Array<{ title: string; location: string }>> =>
+    new Promise((resolve) =>
+      setTimeout(() => {
+        resolve(POST);
+      }, duration)
+    );
+
+  const postQuery = useQuery({
+    queryKey: ["POST"],
+    queryFn: () => wait(1000).then((POST) => POST),
+  });
+
+  if (postQuery.isError) {
+    return <h2>Error in post query</h2>;
+  }
+
+  if (postQuery.isLoading) {
+    return <h2>Loading....</h2>;
+  }
+
   return (
     <>
-      <h2>App</h2>
+      {postQuery.data.map((post) => (
+        <div>
+          <h4>Title: {post.title}</h4>
+          <h4>Location: {post.location}</h4>
+        </div>
+      ))}
     </>
   );
-}
+};
 
 export default App;
